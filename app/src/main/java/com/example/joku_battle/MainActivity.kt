2970018048e81f2fa07ce49e73dc598b8bbf1adb
MinseoSONG <mins041600@naver.com>
2025-01-04 02:06:06 +0900

    [feat] #11 스플래시 UI 구현

--- app/src/main/java/com/example/joku_battle/MainActivity.kt
+++ app/src/main/java/com/example/joku_battle/MainActivity.kt
@@ -9,18 +9,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.joku_battle.core.theme.JoKUbattleTheme
 import com.example.joku_battle.presentation.navigation.MainNavigation
+import com.example.joku_battle.presentation.start.SplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var showSplash by remember { mutableStateOf(true) }
+
             JoKUbattleTheme {
-                MainNavigation()
+                if(showSplash){
+                    SplashScreen {
+                        showSplash = false
+                    }
+                }else{
+                    MainNavigation()
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/joku_battle/presentation/start/SplashScreen.kt
@@ -0,0 +1,62 @@
+package com.example.joku_battle.presentation.start
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.joku_battle.R
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(
+    toHome: () -> Unit
+) {
+    LaunchedEffect(Unit) {
+        delay(2000)
+        toHome()
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.img_home_konkuk),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(30.dp)
+                .size(150.dp)
+        )
+
+        Text(
+            text = stringResource(R.string.splash_title),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 2.dp)
+        )
+        Text(
+            text = stringResource(R.string.splash_subtitle),
+            fontSize = 12.sp
+        )
+    }
+
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun SplashScreenPreview() {
+}
